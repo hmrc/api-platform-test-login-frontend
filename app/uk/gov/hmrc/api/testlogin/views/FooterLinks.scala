b90826336e1d62d530f0ef2a979c5f66efc1c112
Nick Choudhry <28593658+NC-1234@users.noreply.github.com>
2020-10-06 10:53:37 +0100

    APIS-4915: Add footer and links

--- app/uk/gov/hmrc/api/testlogin/views/FooterLinks.scala
+++ app/uk/gov/hmrc/api/testlogin/views/FooterLinks.scala
@@ -14,9 +14,10 @@
  * limitations under the License.
  */
 
+package uk.gov.hmrc.api.testlogin.views
+
 import javax.inject.Inject
 import play.api.i18n.Messages
-import uk.gov.hmrc.api.testlogin.views.FooterConfig
 import uk.gov.hmrc.govukfrontend.views.viewmodels.footer.FooterItem
 
 class FooterLinks @Inject()(appConfig: FooterConfig) {
