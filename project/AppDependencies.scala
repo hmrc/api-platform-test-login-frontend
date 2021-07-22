e8c3fb349c5582c870411ff8fa2ba0a3a917fc16
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2021-07-22 07:34:57 +0100

    APIS-5370 - Upgrade indirect auth-client and fix warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,20 @@ import play.sbt.PlayImport._
 object AppDependencies {
   def apply() = compile ++ test
 
-  lazy val bootstrapPlayVersion = "4.0.0"
-  lazy val playPartialsVersion = "6.11.0-play-26"
-  lazy val hmrcPlayJsonUnionFormatterVersion = "1.11.0"
-  lazy val govUkTemplateVersion = "5.61.0-play-26"
-  lazy val domainVersion = "5.10.0-play-26"
+  private lazy val bootstrapPlayVersion = "4.0.0"
+  private lazy val playPartialsVersion = "6.11.0-play-26"
+  private lazy val hmrcPlayJsonUnionFormatterVersion = "1.11.0"
+  private lazy val govUkTemplateVersion = "5.69.0-play-26"
+  private lazy val domainVersion = "6.1.0-play-26"
 
-  lazy val mockitoVersion = "1.10.19"
-  lazy val scalaTestVersion = "3.0.8"
-  lazy val hmrcTestVersion = "3.9.0-play-26"
-  lazy val scalaTestPlusVersion = "3.1.3"
-  lazy val pegdownVersion = "1.6.0"
-  lazy val wiremockVersion = "1.58"
+  private lazy val mockitoVersion = "1.10.19"
+  private lazy val scalaTestVersion = "3.0.8"
+  private lazy val hmrcTestVersion = "3.9.0-play-26"
+  private lazy val scalaTestPlusVersion = "3.1.3"
+  private lazy val pegdownVersion = "1.6.0"
+  private lazy val wiremockVersion = "1.58"
 
-  lazy val compile = Seq(
+  private lazy val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-26" % bootstrapPlayVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
     "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
@@ -28,9 +28,9 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.57.0-play-26"
   )
 
-  lazy val scope: String = "test, it"
+  private lazy val scope: String = "test, it"
 
-  lazy val test = Seq(
+  private lazy val test = Seq(
     "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusVersion % scope,
     "org.pegdown" % "pegdown" % pegdownVersion % scope,
     "org.jsoup" % "jsoup" % "1.8.1" % scope,
