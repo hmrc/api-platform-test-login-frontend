67b110aa47168bafd592d1722bfc2d179f3332f4
John Green <john.green@digital.hmrc.gov.uk>
2024-10-23 07:35:39 +0100

    API-7906: Upgrade to latest common-domain library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,25 +4,23 @@ import play.sbt.PlayImport._
 
 
 object AppDependencies {
-  val seleniumVersion = "4.8.3"
-
   def apply() = compile ++ test
 
-  lazy val bootstrapVersion    = "9.0.0"
-  lazy val commonDomainVersion = "0.13.0"
+  lazy val bootstrapVersion    = "9.5.0"
+  lazy val commonDomainVersion = "0.17.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"      % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"      % "10.4.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"      % "10.13.0",
     "uk.gov.hmrc"             %% "play-partials-play-30"           % "10.0.0",
     "uk.gov.hmrc"             %% "domain-play-30"                  % "10.0.0",
     "uk.gov.hmrc"             %% "api-platform-common-domain"      % commonDomainVersion
   )
 
   private lazy val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"          % bootstrapVersion,
-    "org.jsoup"               %  "jsoup"                           % "1.15.4",
-    "org.mockito"             %% "mockito-scala-scalatest"         % "1.17.30",
-    "uk.gov.hmrc"             %% "api-platform-test-common-domain" % commonDomainVersion
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"              % bootstrapVersion,
+    "org.jsoup"               %  "jsoup"                               % "1.15.4",
+    "org.mockito"             %% "mockito-scala-scalatest"             % "1.17.30",
+    "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures" % commonDomainVersion
   ).map(_ % "test")
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,12 +4,12 @@ resolvers += Resolver.typesafeRepo("releases")
 
 addSbtPlugin("uk.gov.hmrc"        % "sbt-auto-build"         % "3.22.0")
 addSbtPlugin("uk.gov.hmrc"        % "sbt-distributables"     % "2.5.0")
-addSbtPlugin("org.playframework"  % "sbt-plugin"             % "3.0.3")
+addSbtPlugin("org.playframework"  % "sbt-plugin"             % "3.0.5")
 addSbtPlugin("io.github.irundaia" % "sbt-sassify"            % "1.5.2")
 addSbtPlugin("org.scoverage"      % "sbt-scoverage"          % "2.0.9")
 addSbtPlugin("org.scalastyle"     % "scalastyle-sbt-plugin"  % "1.0.0")
 addSbtPlugin("org.scalameta"      % "sbt-scalafmt"           % "2.5.2")
-addSbtPlugin("ch.epfl.scala"      % "sbt-scalafix"           % "0.11.1")
-addSbtPlugin("ch.epfl.scala"      % "sbt-bloop"              % "1.5.15")
+addSbtPlugin("ch.epfl.scala"      % "sbt-scalafix"           % "0.12.1")
+addSbtPlugin("ch.epfl.scala"      % "sbt-bloop"              % "1.6.0")
 
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
