3bf8491e4f62f953f367d4063812ca8978091119
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-01-29 13:26:16 +0000

    API-8156 - Do the evergreening on top

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,12 @@ import play.sbt.PlayImport._
 object AppDependencies {
   def apply() = compile ++ test
 
-  lazy val bootstrapVersion    = "9.5.0"
+  lazy val bootstrapVersion    = "9.7.0"
   lazy val commonDomainVersion = "0.17.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"      % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"      % "10.13.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"      % "11.11.0",
     "uk.gov.hmrc"             %% "play-partials-play-30"           % "10.0.0",
     "uk.gov.hmrc"             %% "domain-play-30"                  % "10.0.0",
     "uk.gov.hmrc"             %% "api-platform-common-domain"      % commonDomainVersion
